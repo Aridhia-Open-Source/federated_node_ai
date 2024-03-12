38f433710b920ec1dd46a1e47dea8de22b39cc01
Riccardo Casula <riccardocasula@aridhia.net>
2024-03-12 15:27:51 +0000

    FN-1-task: Removed dev files. Changed the keycloak svc namespace url to be a bit more dynamic in the init-job script

--- k8s/federated_node/scripts/job_init.py
+++ k8s/federated_node/scripts/job_init.py
@@ -4,7 +4,8 @@ import json
 import os
 import time
 
-KEYCLOAK_URL = os.getenv("KEYCLOAK_URL", "http://keycloak.keycloak.svc.cluster.local:8080")
+KEYCLOAK_NAMESPACE = os.getenv("KEYCLOAK_NAMESPACE")
+KEYCLOAK_URL = os.getenv("KEYCLOAK_URL", f"http://keycloak.{KEYCLOAK_NAMESPACE}.svc.cluster.local:8080")
 REALM = 'master'
 KEYCLOAK_REALM = os.getenv("KEYCLOAK_REALM", "FederatedNode")
 KEYCLOAK_CLIENT = os.getenv("KEYCLOAK_CLIENT", "global")
