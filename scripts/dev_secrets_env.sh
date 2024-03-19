eaaf5d06a27979bfb0eae418cefb16cc7d222d72
Riccardo Casula <riccardocasula@aridhia.net>
2024-03-19 10:40:55 +0000

    FN-1-task: Fixed an issue with tasks being hardcoded as namespace. tweaked regcreds to create secrets in a loop to avoid repeated code. Added few more lines in the dev_secrets_env to include dynamic namespacing.

--- scripts/dev_secrets_env.sh
+++ scripts/dev_secrets_env.sh
@@ -13,9 +13,19 @@ DEV_ENV_FILE=webserver/dev.env
 if [[ -n $1 ]]; then
     DEV_ENV_FILE=$1
 fi
+KC_NAMESPACE=$(grep -oP '(?<=keycloak:\s).*' k8s/federated_node/dev.values.yaml)
+TASK_NAMESPACE=$(grep -oP '(?<=tasks:\s).*' k8s/federated_node/dev.values.yaml)
+BASE_NAMESPACE=$(helm list -A --output json | jq -r '.[] | select(.name=="federatednode")| .namespace')
 
-PASS=$(kubectl get secret -n keycloak kc-secrets -o json | jq -r '.data.KEYCLOAK_ADMIN_PASSWORD' | base64 -d)
-SEC=$(kubectl get secret -n keycloak kc-secrets -o json | jq -r '.data.KEYCLOAK_GLOBAL_CLIENT_SECRET' | base64 -d)
+if [[ -z $KC_NAMESPACE ]]; then
+    KC_NAMESPACE=$(grep -oP '(?<=keycloak:\s).*' k8s/federated_node/values.yaml)
+fi
+if [[ -z $TASK_NAMESPACE ]]; then
+    TASK_NAMESPACE=$(grep -oP '(?<=tasks:\s).*' k8s/federated_node/dev.values.yaml)
+fi
+
+PASS=$(kubectl get secret -n "${KC_NAMESPACE}" kc-secrets -o json | jq -r '.data.KEYCLOAK_ADMIN_PASSWORD' | base64 -d)
+SEC=$(kubectl get secret -n "${KC_NAMESPACE}" kc-secrets -o json | jq -r '.data.KEYCLOAK_GLOBAL_CLIENT_SECRET' | base64 -d)
 
 if grep 'KEYCLOAK_SECRET=' "$DEV_ENV_FILE"; then
     sed -i "s/KEYCLOAK_SECRET=.*/KEYCLOAK_SECRET=${SEC}/g" "$DEV_ENV_FILE"
@@ -29,5 +39,17 @@ else
     echo "KEYCLOAK_ADMIN_PASSWORD=${PASS}" >> "$DEV_ENV_FILE"
 fi
 
-echo "run kubectl port-forward -n keycloak svc/keycloak 8080"
-echo "and kubectl port-forward svc/db 5432"
+if grep 'TASK_NAMESPACE=' "$DEV_ENV_FILE"; then
+    sed -i "s/TASK_NAMESPACE=.*/TASK_NAMESPACE=${TASK_NAMESPACE}/g" "$DEV_ENV_FILE"
+else
+    echo "TASK_NAMESPACE=${TASK_NAMESPACE}" >> "$DEV_ENV_FILE"
+fi
+
+if grep 'KEYCLOAK_NAMESPACE=' "$DEV_ENV_FILE"; then
+    sed -i "s/KEYCLOAK_NAMESPACE=.*/KEYCLOAK_NAMESPACE=${KC_NAMESPACE}/g" "$DEV_ENV_FILE"
+else
+    echo "KEYCLOAK_NAMESPACE=${KC_NAMESPACE}" >> "$DEV_ENV_FILE"
+fi
+
+echo "run kubectl port-forward -n ${KC_NAMESPACE} svc/keycloak 8080"
+echo "and kubectl port-forward -n ${BASE_NAMESPACE} svc/db 5432"
