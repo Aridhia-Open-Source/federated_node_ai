bd589bc98f39e1930d46868b6c983a2f998ccf33
r-casula <riccardo.casula@aridhia.com>
2025-09-02 12:15:08 +0100

    192-kc-login-issue: Removed credentials reset job, and moved as init container for keycloak. Adjusted var names in reset script. Added an import_microk8s script for dev QoL

--- build/alpine/keycloak-reset.sh
+++ build/alpine/keycloak-reset.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "DB info:"
-echo "Host: ${PGHOST}"
-echo "User: ${PGUSER}"
-echo "DB: ${PGDATABASE}"
+echo "Host: ${KC_DB_URL_HOST}"
+echo "User: ${KC_DB_USERNAME}"
+echo "DB: ${KC_DB_URL_DATABASE}"
 
-psql -d "${PGDATABASE}" -h "${PGHOST}" -U "${PGUSER}" -f - <<SQL
+psql -d "${KC_DB_URL_DATABASE}" -h "${KC_DB_URL_HOST}" -U "${KC_DB_USERNAME}" -f - <<SQL
     DELETE FROM credential WHERE user_id IN (SELECT id FROM user_entity WHERE username = 'admin');
     DELETE FROM user_role_mapping WHERE user_id IN (SELECT id FROM user_entity WHERE username = 'admin');
     DELETE FROM user_entity WHERE username = 'admin';
